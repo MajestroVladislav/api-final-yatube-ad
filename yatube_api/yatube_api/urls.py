e5501a806649117f9b5338f02b856edc6bd9b4cd
MajestroVladislav <vlakal1nin@yandex.ru>
2026-03-25 21:12:44 +0400

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -6,17 +6,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
-from django.views.generic import TemplateView # Если используете для redoc
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')), # Ваши основные эндпоинты API
-
-    # Эндпоинты для JWT-аутентификации (simplejwt)
-    # Они должны соответствовать путям, которые ищут тесты:
-    # /api/v1/jwt/create/
-    # /api/v1/jwt/refresh/
-    # /api/v1/jwt/verify/
+    path('api/v1/', include('api.urls')),
     path('api/v1/jwt/create/', TokenObtainPairView.as_view(), name='jwt_create'),
     path('api/v1/jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh'),
     path('api/v1/jwt/verify/', TokenVerifyView.as_view(), name='jwt_verify'),
